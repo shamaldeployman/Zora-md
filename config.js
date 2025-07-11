a6c9ee6d13630541ba1722043de9ed5f0db3739f
shamaldeployman <shamdeploy@gmail.com>
2025-07-11 20:14:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
   return text === fault ? true : false;
 };
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "", // SESSION_ID is required to start the bot. It must begin with 'ZORA~', Example: ZORA~youruniquesessionid
+  SESSION_ID: process.env.SESSION_ID || "ZORA~Wc1AiTbA#rnNWn4i4vY3uY8saQ0jzyKvubdTUf2v_y8O2PutIZ3Q", // SESSION_ID is required to start the bot. It must begin with 'ZORA~', Example: ZORA~youruniquesessionid
   MONGODB: process.env.MONGODB || ""   // MONGODB URL is optional. Add it only if you have a valid MongoDB URI. If you don't have one, leave it empty. Do NOT insert invalid URLs – the bot won't start !!
 };
